741942b0c9836cc8f636abe721864c17713166a8
Oscar Ariel Saucedo <arielsaucedo@gmail.com>
2021-08-28 15:54:36 -0300

    The Logo component is created

---
+++ src/components/UI/Navbar/Logo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Box, Image, Stack } from '@chakra-ui/react';
+
+const Logo = () => {
+  return (
+    <Stack>
+      <Image height="20px" src="../../assets/logo.svg" width="140px" />
+    </Stack>
+  );
+};
+
+export default Logo;
