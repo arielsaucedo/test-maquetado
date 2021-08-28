67732732df9475bde2972107ea6412e77b6e12e5
Oscar Ariel Saucedo <arielsaucedo@gmail.com>
2021-08-28 11:57:40 -0300

    Initial project setup - Eslint, Prettier and Chakra UI - Deleting unnecessary files

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        printWidth: 100,
+        singleQuote: true,
+        trailingComma: 'all',
+        semi: true,
+        endOfLine: 'auto',
+        bracketSpacing: true,
+        arrowParens: 'always',
+      },
+    ],
+    'react/prop-types': 0,
+    'no-console': 'warn',
+    'react/jsx-sort-props': [
+      'warn',
+      {
+        callbacksLast: true,
+        shorthandFirst: true,
+        noSortAlphabetically: false,
+        reservedFirst: true,
+      },
+    ],
+  },
+};
